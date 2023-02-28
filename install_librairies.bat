1d9c029856d2955e6a3567b4284509de701164c1
Alex <112651933+Arkemmm@users.noreply.github.com>
2023-02-28 17:53:29 +0100

    Add files via upload

---
+++ install_librairies.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM Mettre à jour pip
+py -m pip install --upgrade pip
+
+REM Installation des bibliotheques...
+py -m pip install pyinstaller
+py -m pip install keyboard
+
+REM Verification...
+if %errorlevel% neq 0 (
+    color 0a
+    echo Une erreur est survenue.
+) else (
+    color 0f
+    echo Installation des bibliotheques Python reussi !
+)
+
+pause
