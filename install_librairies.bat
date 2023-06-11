f72be1b50d91a8ab843a9069b236d3be810b49ce
Alex <112651933+Arkemmm@users.noreply.github.com>
2023-06-11 19:41:04 +0200

    Update install_librairies.bat

--- install_librairies.bat
+++ install_librairies.bat
@@ -12,12 +12,23 @@ cls
 REM Turn off command echoing
 echo off
 
-echo ██╗░░██╗███████╗██╗░░░██╗░██╗░░░░░░░██╗░█████╗░████████╗░█████╗░██╗░░██╗
-echo ██║░██╔╝██╔════╝╚██╗░██╔╝░██║░░██╗░░██║██╔══██╗╚══██╔══╝██╔══██╗██║░░██║
-echo █████═╝░█████╗░░░╚████╔╝░░╚██╗████╗██╔╝███████║░░░██║░░░██║░░╚═╝███████║
-echo ██╔═██╗░██╔══╝░░░░╚██╔╝░░░░████╔═████║░██╔══██║░░░██║░░░██║░░██╗██╔══██║
-echo ██║░╚██╗███████╗░░░██║░░░░░╚██╔╝░╚██╔╝░██║░░██║░░░██║░░░╚█████╔╝██║░░██║
-echo ╚═╝░░╚═╝╚══════╝░░░╚═╝░░░░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝
+echo  ,ggg,        gg                                                                               
+echo dP""Y8b       dP                                                     I8              ,dPYb,    
+echo Yb, `88      d8'                                                     I8              IP'`Yb    
+echo  `"  88    ,dP'                                                   88888888           I8  8I    
+echo      88aaad8"                                                        I8              I8  8'    
+echo      88""""Yb,     ,ggg,   gg     gg  gg    gg    gg     ,gggg,gg    I8      ,gggg,  I8 dPgg,  
+echo      88     "8b   i8" "8i  I8     8I  I8    I8    88bg  dP"  "Y8I    I8     dP"  "Yb I8dP" "8I 
+echo      88      `8i  I8, ,8I  I8,   ,8I  I8    I8    8I   i8'    ,8I   ,I8,   i8'       I8P    I8 
+echo      88       Yb, `YbadP' ,d8b, ,d8I ,d8,  ,d8,  ,8I  ,d8,   ,d8b, ,d88b, ,d8,_    _,d8     I8,
+echo      88        Y8888P"Y888P""Y88P"888P""Y88P""Y88P"   P"Y8888P"`Y888P""Y88P""Y8888PP88P     `Y8
+echo                                 ,d8I'                                                          
+echo                               ,dP'8I                                                           
+echo                              ,8"  8I                                                           
+echo                              I8   8I                                                           
+echo                              `8, ,8I                                                           
+echo                               `Y8P"                                                            
+
 
 REM Update pip
 echo Updating pip...
